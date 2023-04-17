1a89e37d3010e3036a6ed6b8fbeebe777520804b
kaildrai <matthew.pinkus3@gmail.com>
2023-04-17 13:18:42 +1000

    (tmux) fixed issues with theme

--- nvim/after/plugin/neosolarized.rc.lua
+++ nvim/after/plugin/neosolarized.rc.lua
@@ -1,27 +1,7 @@
-local status, n = pcall(require, 'neosolarized')
-if (not status) then end
-
-n.setup({ commit_italics = true })
-
-local colorbuddy = require('colorbuddy.init')
-local Color = colorbuddy.Color
-local colors = colorbuddy.colors
-local Group = colorbuddy.Group
-local groups = colorbuddy.groups
-local styles = colorbuddy.styles
-
-Color.new('white', '#ffffff')
-Color.new('black', '#000000')
-Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
-Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
-Group.new('Visual', colors.none, colors.base03, styles.reverse)
-
-local cError = groups.Error.fg
-local cInfo = groups.Information.fglocal status, n = pcall(require, "neosolarized")
+local status, n = pcall(require, "neosolarized")
 if (not status) then return end
-
 n.setup({
-  comment_italics = true,
+  comment_italics =true,
 })
 
 local cb = require('colorbuddy.init')
@@ -33,7 +13,7 @@ local styles = cb.styles
 
 Color.new('white', '#ffffff')
 Color.new('black', '#000000')
-Group.new('Normal', colors.base1, colors.NONE, styles.NONE)
+Group.new('Normal', colors.base1, colors.NONE, styles.NONE, colors.base1)
 Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
 Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
 Group.new('Visual', colors.none, colors.base03, styles.reverse)
@@ -47,22 +27,10 @@ Group.new("DiagnosticVirtualTextError", cError, cError:dark():dark():dark():dark
 Group.new("DiagnosticVirtualTextInfo", cInfo, cInfo:dark():dark():dark(), styles.NONE)
 Group.new("DiagnosticVirtualTextWarn", cWarn, cWarn:dark():dark():dark(), styles.NONE)
 Group.new("DiagnosticVirtualTextHint", cHint, cHint:dark():dark():dark(), styles.NONE)
+
 Group.new("DiagnosticUnderlineError", colors.none, colors.none, styles.undercurl, cError)
 Group.new("DiagnosticUnderlineWarn", colors.none, colors.none, styles.undercurl, cWarn)
 Group.new("DiagnosticUnderlineInfo", colors.none, colors.none, styles.undercurl, cInfo)
 Group.new("DiagnosticUnderlineHint", colors.none, colors.none, styles.undercurl, cHint)
-
-Group.new("HoverBorder", colors.yellow, colors.none, styles.NONE)
-local cWarn = groups.Warning.fg
-local cHint = groups.Hint.fg
-
-Group.new('DiagnosticVirtualTextError', cError, cError:dark():dark():dark():dark(), styles.NONE)
-Group.new('DiagnosticVirtualTextInfo', cInfo, cInfo:dark():dark():dark(), styles.NONE)
-Group.new('DiagnosticVirtualTextWarn', cWarn, cWarn:dark():dark():dark(), styles.NONE)
-Group.new('DiagnosticVirtualTextHint', cHint, cHint:dark():dark():dark(), styles.NONE)
-Group.new('DiagnosticUnderlineError', colors.none, colors.none, styles.undercurl, cError)
-Group.new('DiagnosticUnderlineWarn', colors.none, colors.none, styles.undercurl, cWarn)
-Group.new('DiagnosticUnderlineInfo', colors.none, colors.none, styles.undercurl, cInfo)
-Group.new('DiagnosticUnderlineHint', colors.none, colors.none, styles.undercurl, cHint)
 
 Group.new("HoverBorder", colors.yellow, colors.none, styles.NONE)
